ff0f028f9a581777aa7c5caa846a83a30dad41dc
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-15 05:52:11 +0000

    feat(core): fix payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e9ac395077b4aceaf55da39238119d9 = () => {
-  return '2025-02-12T07:56:16Z';
+const node_eebb95990bc546a3b7b344135512ae79 = () => {
+  return '2025-02-15T05:52:11Z';
 };

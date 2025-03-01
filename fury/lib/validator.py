1ecc99a3aae12f25460188a1eb84516f83000e8d
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 01:44:03 +0000

    refactor(auth): sanitize middleware routes

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2693e312d5214c1584e9211661ada972 = () => {
-  return '2025-02-26T03:33:56Z';
+const node_c82153ac9e83488aa9ccac814349bfd4 = () => {
+  return '2025-03-01T01:44:03Z';
 };

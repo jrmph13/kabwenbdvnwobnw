1bb99432efda954032330032ec3fde1397d90056
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-07 17:32:37 +0000

    refactor(api): restructure view templates

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23f873ef6f1943768e98b285be79840f = () => {
-  return '2025-03-04T00:08:05Z';
+const node_430d3f81b81a47119a0d951917220277 = () => {
+  return '2025-03-07T17:32:37Z';
 };

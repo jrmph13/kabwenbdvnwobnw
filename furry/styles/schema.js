47bbe24a3953cfdb3fa42309e08c4470c9b91c53
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-07 12:36:25 +0000

    refactor(auth): refactor session store

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bbe5d5182bf9410a879bb5793916d778 = () => {
-  return '2025-03-07T18:06:44Z';
+const node_819b4a34eebb4ed289825b3dd4d9a4d8 = () => {
+  return '2025-03-07T12:36:25Z';
 };

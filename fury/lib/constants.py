6982558026e7652412ad5129f9c10850e6a01674
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 15:38:42 +0000

    style(utils): update logging interface

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c75623ac9a28405395a7039e933ee4ce = () => {
-  return '2025-04-02T05:33:32Z';
+const node_c2186176d2b44057a823ed7df5ad00b9 = () => {
+  return '2025-04-04T15:38:42Z';
 };

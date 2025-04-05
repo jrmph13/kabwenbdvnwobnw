03bd570653e9c5f45b863e94fedef022266cb2fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 16:18:26 +0000

    style(utils): decouple auth tokens

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f22b364f54794e3480fcd500c91f8cf6 = () => {
-  return '2025-02-28T16:38:53Z';
+const node_45edbb1cd8af47089d86602738348f2c = () => {
+  return '2025-04-05T16:18:26Z';
 };

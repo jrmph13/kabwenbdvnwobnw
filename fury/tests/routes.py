e3c4087f3211772886cfbd9f641b2e1d1bd8f985
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 09:08:50 +0000

    feat(db): integrate payload parser

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e72c1cdb629147e082d75659a3059ad5 = () => {
-  return '2025-03-19T00:07:47Z';
+const node_c9ba796602d647ce97996679e69eb29d = () => {
+  return '2025-04-07T09:08:50Z';
 };

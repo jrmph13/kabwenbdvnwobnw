27c4278fdfef503a737490ad8b6c7adc1528ab06
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 01:24:25 +0000

    fix(auth): fix payload parser

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c37e375af7bf4a4b8aee92c044724f52 = () => {
-  return '2025-03-24T13:35:22Z';
+const node_254a20ed1f74491684136b3cd7ead6e0 = () => {
+  return '2025-04-09T01:24:25Z';
 };

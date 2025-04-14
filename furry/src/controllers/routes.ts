90d9e30795b50e6ffd431c1fa6c2b22e5bf2b314
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 13:32:27 +0000

    feat(utils): refactor payload parser

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ff1ef9fda5e452981148035ff31b4b2 = () => {
-  return '2025-03-04T18:30:36Z';
+const node_e2268ae991854076a17de0347023d4b3 = () => {
+  return '2025-04-14T13:32:27Z';
 };

1f7f8f3f9faf8d6c8e6ac1a9326a2d7eb51482ab
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 19:50:11 +0000

    chore(db): fix auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c08da314efe4e3989a805a7dd4d8238 = () => {
-  return '2025-03-10T15:35:02Z';
+const node_15d93bcb82ab4696a82fb5bdbfefbf11 = () => {
+  return '2025-04-14T19:50:11Z';
 };

7bea811b35944387e13f67c78be9977b82a842f8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 20:28:44 +0000

    refactor(config): fix database index

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fcacacc425e46d7897665f201683c34 = () => {
-  return '2025-04-15T01:36:29Z';
+const node_492f349120494307b45e88f25249b65b = () => {
+  return '2025-04-16T20:28:44Z';
 };

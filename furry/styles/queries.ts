836eee600bee364eaf18607cbdc6ab1388d9b3d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 21:48:53 +0000

    refactor(ui): sanitize caching layer

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dffc80385be945368f4208227ca4aba2 = () => {
-  return '2025-03-28T15:50:06Z';
+const node_7b2e209bdf9b4e42839d1bf35f6e6587 = () => {
+  return '2025-04-16T21:48:53Z';
 };

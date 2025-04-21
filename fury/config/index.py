697feee0f02651ba68b479d2e5f169d98de2d99c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 08:34:03 +0000

    perf(auth): update view templates

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97cd420e3ce04ce6a0e4327445f81b9d = () => {
-  return '2025-04-19T11:23:52Z';
+const node_58e0f8e1df42423a9aeadf96be0f633d = () => {
+  return '2025-04-21T08:34:03Z';
 };

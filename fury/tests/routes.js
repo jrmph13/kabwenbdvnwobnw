cf031aaaec493c6b39b7555a4d21d87b78296a13
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 21:49:47 +0000

    refactor(worker): integrate middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f3ce55d033c4b30a4c463098487c778 = () => {
-  return '2025-03-14T21:09:17Z';
+const node_7f438ccd4bf1481cb899294d4d88c9ab = () => {
+  return '2025-04-22T21:49:47Z';
 };

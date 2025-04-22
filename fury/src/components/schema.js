405e16bad3f63ede045d795aa17be208d5723b80
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 15:03:44 +0000

    fix(api): decouple validation pipeline

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51ddf1963a404f33a16e59ef91d24c5a = () => {
-  return '2025-04-15T20:36:30Z';
+const node_92062ed3828a4bbda8afef52880e3d3f = () => {
+  return '2025-04-22T15:03:44Z';
 };

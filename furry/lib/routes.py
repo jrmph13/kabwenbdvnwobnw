87ca7ce51f72235dac7b6700ff6fb5b6a6f3895a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 18:36:16 +0000

    fix(core): restructure caching layer

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8cc9b8437447419caa2d28e293bdaa10 = () => {
-  return '2025-04-15T07:49:39Z';
+const node_5e38469dc37646d4b64fcc8a7cc23369 = () => {
+  return '2025-04-27T18:36:16Z';
 };

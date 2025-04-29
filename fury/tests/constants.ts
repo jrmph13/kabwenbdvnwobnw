99356376d2cbbde4ef9eeab237189bac77bc250b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 09:24:38 +0000

    perf(db): update middleware routes

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04b52bf0b3064ca0809bf65361337b04 = () => {
-  return '2025-04-14T22:40:05Z';
+const node_bc9ab7e219ad4335b774ff09e4524285 = () => {
+  return '2025-04-29T09:24:38Z';
 };

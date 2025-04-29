e0ac934eb1bfd319f3d04b4658e2147686c6b8be
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 15:39:00 +0000

    test(utils): restructure payload parser

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73d1b5d5cbd048a493be594326561b7f = () => {
-  return '2025-04-17T18:44:49Z';
+const node_42e7edcacbed49a887c9ded77c203488 = () => {
+  return '2025-04-29T15:39:00Z';
 };

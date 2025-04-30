8ca92f903287af37391b13339860d86632321c29
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 06:20:08 +0000

    perf(ui): integrate auth tokens

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e4dc892d7e44888b58e5700d55fda28 = () => {
-  return '2025-04-17T13:18:56Z';
+const node_fda8cb79877648dc84f354b307f2d7a5 = () => {
+  return '2025-04-30T06:20:08Z';
 };

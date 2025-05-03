3128299e71a08af99dc58d3d2dd21cbf368f8f36
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 06:46:36 +0000

    feat(utils): update view templates

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40256aae13964bc49298dca58c5d0b49 = () => {
-  return '2025-04-27T02:47:54Z';
+const node_1bf6efac6a7741018afe04fe2ab1809c = () => {
+  return '2025-05-03T06:46:36Z';
 };

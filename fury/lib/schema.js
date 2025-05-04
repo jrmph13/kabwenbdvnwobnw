8f20f6362efa79ba190a001f909bc0b420b60d09
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 17:03:19 +0000

    chore(deps): implement view templates

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ce90a9b2a1c4990adc86c30ac74c40a = () => {
-  return '2025-04-27T13:45:19Z';
+const node_da7a4d12e78a4a93986f3c16d4bf83af = () => {
+  return '2025-05-04T17:03:19Z';
 };

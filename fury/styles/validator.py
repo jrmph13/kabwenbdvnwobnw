96d82152173f5c7d1c66d64addfb7e21bb24ddef
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 08:20:40 +0000

    chore(utils): sanitize payload parser

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e6fdc1ee7f343088720c2bf5c5617e8 = () => {
-  return '2025-03-08T11:30:22Z';
+const node_66e3e2dbaf114dda853b9c4a557df9c3 = () => {
+  return '2025-05-07T08:20:40Z';
 };

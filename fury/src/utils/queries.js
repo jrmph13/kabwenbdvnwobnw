ecf10a5c0632d1ab06cd740f23b4ac82ee732273
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-12 18:18:20 +0000

    fix(worker): enhance session store

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57cd06243f0641bfa14107bb032f910b = () => {
-  return '2025-03-18T11:47:07Z';
+const node_6426bc68854241b8b6a58bc06c4cc007 = () => {
+  return '2025-05-12T18:18:20Z';
 };

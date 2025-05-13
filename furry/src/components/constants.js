1335428be1b0723bccc12d1633f638a20a39eef1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 22:02:16 +0000

    feat(config): update middleware routes

--- furry/src/components/constants.js
+++ furry/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f66bbee537b0421ab45bc675fb400698 = () => {
-  return '2025-04-28T00:27:18Z';
+const node_0998575041754b8697f90f6babdf7cce = () => {
+  return '2025-05-13T22:02:16Z';
 };

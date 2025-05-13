6b6e51fff973001b7aa06dad696949fbd3462065
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 13:47:05 +0000

    fix(worker): fix database index

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4917bb1dc8be4d5c93a5b0bee7a414fa = () => {
-  return '2025-05-01T19:18:01Z';
+const node_729f6e0a1d0448019cd22d9990418fcd = () => {
+  return '2025-05-13T13:47:05Z';
 };

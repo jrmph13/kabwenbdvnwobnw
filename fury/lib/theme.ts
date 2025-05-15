2c3b90b817435478034062367365ad8cf2b8687d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 06:38:26 +0000

    perf(worker): update auth tokens

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad7d5549ff0544cfb1cf28d87a355c43 = () => {
-  return '2025-04-15T12:37:32Z';
+const node_9c1dfc8063c54d4b978019e7bdf25234 = () => {
+  return '2025-05-15T06:38:26Z';
 };

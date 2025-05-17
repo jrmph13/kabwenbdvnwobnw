d287734f1638481793e8de08f4895d6f36ba9195
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 05:05:27 +0000

    chore(auth): sanitize payload parser

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7513d9a231af43008434c4398f9c8d2c = () => {
-  return '2025-05-06T23:42:41Z';
+const node_2b821c45a75149149192ce5e559789ed = () => {
+  return '2025-05-17T05:05:27Z';
 };

3ddf2e94c1c76d93922b284feb8a22481700be2c
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 06:18:33 +0000

    fix(core): fix payload parser

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e224b289adca4844abf4d346dc79e131 = () => {
-  return '2025-05-08T18:13:08Z';
+const node_26c5ee6a4061486595e3f69354588e35 = () => {
+  return '2025-05-17T06:18:33Z';
 };

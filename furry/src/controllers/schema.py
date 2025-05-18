14b22fdd49bcc621ed3f7d808d2cae379b3fc6a1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 10:33:00 +0000

    feat(api): fix session store

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c95e9e85c4c417f8fbd3e429bc4562f = () => {
-  return '2025-05-17T23:54:55Z';
+const node_9eb76ff20eb84e8db75038ebc365ade7 = () => {
+  return '2025-05-18T10:33:00Z';
 };

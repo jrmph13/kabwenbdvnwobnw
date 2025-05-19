56d32f63a208c5d8110f086c23898ee9264b8ea8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 00:58:08 +0000

    test(auth): restructure payload parser

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b821c45a75149149192ce5e559789ed = () => {
-  return '2025-05-17T05:05:27Z';
+const node_d4b482de5dc3462589eb6904177ac584 = () => {
+  return '2025-05-19T00:58:08Z';
 };

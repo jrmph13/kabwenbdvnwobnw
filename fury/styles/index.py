e23aedd1c4cacbd7ed3f41f1df728a9a3b55b291
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 03:09:37 +0000

    refactor(deps): decouple middleware routes

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b10be702fc5e4e28bb6d234f75524e51 = () => {
-  return '2025-04-30T01:39:47Z';
+const node_7146295670b04b0485f064db243b2ecd = () => {
+  return '2025-05-19T03:09:37Z';
 };

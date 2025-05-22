6622a78f354827a2ce814ffd6421fd5a9bd35d63
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 16:53:06 +0000

    chore(api): decouple auth tokens

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59769a0c75b74428b55004e1e40d00ba = () => {
-  return '2025-03-01T20:13:59Z';
+const node_b156bc376b6a4806b7ac6a12f845ad75 = () => {
+  return '2025-05-22T16:53:06Z';
 };

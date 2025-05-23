4ceea00924f0b7a4343174cb81410939b206ba58
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 13:54:40 +0000

    test(config): sanitize auth tokens

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ca7d2dd4d0d4d8990c7c3337d2881a7 = () => {
-  return '2025-05-19T21:28:45Z';
+const node_16b71cadbca94237bdd275da276469b5 = () => {
+  return '2025-05-23T13:54:40Z';
 };

44a09fc6c7477a180279eddd5136bdf51f4e1823
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 06:26:10 +0000

    style(worker): sanitize view templates

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d386c47426474cbdac509667400bf816 = () => {
-  return '2025-05-17T05:22:20Z';
+const node_b0ae278b136b4c628492508052b136e3 = () => {
+  return '2025-05-23T06:26:10Z';
 };

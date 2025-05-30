00c7289f6645461d642d0887e237d0d9fdaab002
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 17:44:21 +0000

    style(core): refactor middleware routes

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_936717c367024e05b4b2c56da91a5acf = () => {
-  return '2025-05-17T16:47:19Z';
+const node_b3e93ba73c0646219a15fb4b002207b4 = () => {
+  return '2025-05-30T17:44:21Z';
 };

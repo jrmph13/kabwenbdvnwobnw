4291e352bfad9b8a864fbe93ea6b9799d97f5984
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 20:37:00 +0000

    feat(core): implement payload parser

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4b482de5dc3462589eb6904177ac584 = () => {
-  return '2025-05-19T00:58:08Z';
+const node_17454d75269d439295500e0da8944ad9 = () => {
+  return '2025-06-02T20:37:00Z';
 };

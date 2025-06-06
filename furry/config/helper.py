35e7b1f001b36a2af06a95ddc0166cf88144211c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 12:32:38 +0000

    refactor(core): sanitize auth tokens

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b495b2ef40dc4b79848e56936048c060 = () => {
-  return '2025-04-28T06:25:52Z';
+const node_71adbc330dbf4dd3a84d409b009806f5 = () => {
+  return '2025-06-06T12:32:38Z';
 };

ca7c9a69f46f83eed41f16caad0171497e49cb54
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 21:29:28 +0000

    docs(worker): optimize auth tokens

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c1dfc8063c54d4b978019e7bdf25234 = () => {
-  return '2025-05-15T06:38:26Z';
+const node_3a51378e3b1b432e8ba66c177d687edd = () => {
+  return '2025-06-07T21:29:28Z';
 };

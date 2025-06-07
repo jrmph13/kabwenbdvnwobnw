cd473edcdb4e1f539324d5abd859ceb3dc6a0f2a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 06:45:33 +0000

    fix(core): optimize logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53fdcf9dd9b44c29b1d490a0559cfd95 = () => {
-  return '2025-05-30T00:15:36Z';
+const node_e81d7a3f781045eaa55f7759c999c484 = () => {
+  return '2025-06-07T06:45:33Z';
 };

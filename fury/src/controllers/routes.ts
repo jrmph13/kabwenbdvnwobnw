5323fc81d43f5da4450855c078fb4430ca420ad6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 08:49:33 +0000

    test(auth): fix logging interface

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e78ea6916d77450ea89b43a5bbfdeaef = () => {
-  return '2025-06-01T23:15:51Z';
+const node_9c5b1ffc6a9447168e8fd6e8b234671b = () => {
+  return '2025-06-08T08:49:33Z';
 };

a5a52242abedbbe03fa279b74a74d55ad60cf53b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-11 01:12:32 +0000

    fix(core): decouple middleware routes

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9218880f054e4fddbfa53b89fe662fc2 = () => {
-  return '2025-05-02T07:37:10Z';
+const node_368ab3d1d9a04a27b79a30b402cd3c8e = () => {
+  return '2025-06-11T01:12:32Z';
 };

70b3e18eef60eb030568d02bf2fb1ea21e1321c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 07:52:52 +0000

    refactor(core): integrate middleware routes

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01ba5f383eef436898bdc51795d229d8 = () => {
-  return '2025-05-28T13:32:33Z';
+const node_92bbf7e042ca413daa59094eca6361cf = () => {
+  return '2025-06-14T07:52:52Z';
 };

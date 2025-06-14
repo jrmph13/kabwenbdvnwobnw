af6cd5c2faffb6b5d7bc317187edef033b59110c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 09:39:23 +0000

    perf(api): sanitize session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db09188ef402431b89d3bdc5c0504ca0 = () => {
-  return '2025-05-23T15:47:03Z';
+const node_6ffcbde0b75146789b876f97048c5568 = () => {
+  return '2025-06-14T09:39:23Z';
 };

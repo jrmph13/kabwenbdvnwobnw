afe5bc8ee2e8fa4e1cbf4a83f9236aebf530876b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 04:27:49 +0000

    fix(ui): refactor auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3da122b5f0bd49aabc1bc01b6e5810c9 = () => {
-  return '2025-03-28T10:23:13Z';
+const node_098f1fc94f384ab4a1bdd2d3a77610e4 = () => {
+  return '2025-06-15T04:27:49Z';
 };

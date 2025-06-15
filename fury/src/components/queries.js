5a75d17005666ef8eca272126e8aaf1b795bde87
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 16:04:09 +0000

    fix(api): sanitize middleware routes

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60bcec3a4736477b90152bb4b34f4d1c = () => {
-  return '2025-03-20T00:38:05Z';
+const node_120d54d3019942f6b0b6fda1107f834b = () => {
+  return '2025-06-15T16:04:09Z';
 };

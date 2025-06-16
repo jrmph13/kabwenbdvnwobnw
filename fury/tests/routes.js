30e5d7098b8273a4ead355900b12adb1513f4fd7
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 21:27:40 +0000

    fix(utils): sanitize view templates

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83edcc50a50648d397845b4fa31acc12 = () => {
-  return '2025-06-06T15:34:39Z';
+const node_7c2309334e20473aaea717929a77fa1d = () => {
+  return '2025-06-16T21:27:40Z';
 };

40229110c322f6125187ca0fb327d112b46d757b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 07:30:19 +0000

    feat(worker): refactor payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c128c8261c74c188420eb4f3e9ac24c = () => {
-  return '2025-05-20T22:53:59Z';
+const node_e5b23296b40e4550be673912743b8d95 = () => {
+  return '2025-06-16T07:30:19Z';
 };

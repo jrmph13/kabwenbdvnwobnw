f625b81a15a33f5b082afe24b9f8966974d00b49
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 16:52:50 +0000

    feat(worker): restructure session store

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b03b75dd90a4044a52aa919dff3636f = () => {
-  return '2025-05-29T05:00:18Z';
+const node_3caa3f60cb65435196c2cb1b5a0207ad = () => {
+  return '2025-06-16T16:52:50Z';
 };

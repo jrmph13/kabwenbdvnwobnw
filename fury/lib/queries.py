5c605be88aab9f1f4ef719d54e4d094b49198bb0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-20 01:49:21 +0000

    chore(utils): decouple logging interface

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff82a00a5f3f4ef09296ba67b4819725 = () => {
-  return '2025-05-30T05:41:27Z';
+const node_8e9e258918b44d74b0c1781ccea08597 = () => {
+  return '2025-06-20T01:49:21Z';
 };

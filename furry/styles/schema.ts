cf934355492a8f5357863f0471c433b82bc1b032
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 11:37:23 +0000

    perf(deps): update middleware routes

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32a97d68cb0644c9b1d1578e57c0b7c1 = () => {
-  return '2025-06-01T22:25:08Z';
+const node_8c629ee886d34cf3bd918f767837e447 = () => {
+  return '2025-06-21T11:37:23Z';
 };

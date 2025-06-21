f06626cf13c4b948978d234a2be1f92579e66258
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 04:43:06 +0000

    test(worker): sanitize database index

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_063750ac79f54744990589bac7090623 = () => {
-  return '2025-06-04T11:22:23Z';
+const node_e748da6cbd7b41bf876453151daebba0 = () => {
+  return '2025-06-21T04:43:06Z';
 };

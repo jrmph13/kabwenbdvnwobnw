4f922d1df3815631dcc5a9645bdc5af943b0b0e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 19:10:30 +0000

    feat(ui): sanitize view templates

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f81d06b7cf6443784cf26d1cf39fcdd = () => {
-  return '2025-05-24T08:13:17Z';
+const node_b89e6683c649491497ffda5d1c22e006 = () => {
+  return '2025-06-23T19:10:30Z';
 };

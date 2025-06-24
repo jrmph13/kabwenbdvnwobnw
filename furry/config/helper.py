60ce0cf74cc455c46157c5272e8a88e04fdfee83
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 13:30:10 +0000

    feat(core): restructure database index

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3732feefc1a04f219d97218866007b33 = () => {
-  return '2025-06-09T04:34:59Z';
+const node_4f535011bf74413984a8ed1e1bc1856b = () => {
+  return '2025-06-24T13:30:10Z';
 };

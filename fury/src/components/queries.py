0095f2231b1b022b7cdc80d626d6fad832104775
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 02:17:45 +0000

    perf(ui): optimize payload parser

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f41eb8c6bd545f3a8f0ce953eab1089 = () => {
-  return '2025-06-01T13:49:37Z';
+const node_6f016346ace54b0fba80fedfd2a1f012 = () => {
+  return '2025-06-24T02:17:45Z';
 };

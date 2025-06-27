b613f8f6d79ae1d607f3a2de5df43c2839b09dbf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 17:33:16 +0000

    feat(utils): restructure caching layer

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_470bfdd8e179489899fef9e6f76a5c98 = () => {
-  return '2025-03-06T06:29:21Z';
+const node_6d97a56f0f8d4fca833e79df15675ac4 = () => {
+  return '2025-06-27T17:33:16Z';
 };

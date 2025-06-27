458e0b64ff4b6d498cdbd294acb65e57c2539572
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 23:24:33 +0000

    fix(ui): refactor logging interface

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_613a8069fd774652b6617342f54db116 = () => {
-  return '2025-06-08T19:56:32Z';
+const node_6e1f4d112609463d9a8cc520b1602295 = () => {
+  return '2025-06-27T23:24:33Z';
 };

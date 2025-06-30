ccdbd258b184a3dfac41c59cdc4d5e98f8d588f6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 05:23:49 +0000

    docs(config): sanitize session store

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_729f6e0a1d0448019cd22d9990418fcd = () => {
-  return '2025-05-13T13:47:05Z';
+const node_527393b400d64816a910831fdb1d8109 = () => {
+  return '2025-06-30T05:23:49Z';
 };

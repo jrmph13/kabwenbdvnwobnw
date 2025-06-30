32233e72fc3bdbb109af6b330a300ea4b6762cce
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 06:13:50 +0000

    fix(db): sanitize view templates

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_700f5874534d411181c89adac7c6e706 = () => {
-  return '2025-05-19T05:50:02Z';
+const node_7e48bfe3690a43bc80c4dde7816a362e = () => {
+  return '2025-06-30T06:13:50Z';
 };

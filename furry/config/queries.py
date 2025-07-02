a766998d02dfe9a762960f88c1eac6eeead8e407
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 09:57:34 +0000

    style(db): optimize session store

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f6a2e4b02904d748c467b0b08c884c7 = () => {
-  return '2025-03-30T15:56:10Z';
+const node_eebbb3d048c742fdb8536a7347f6dab5 = () => {
+  return '2025-07-02T09:57:34Z';
 };

4e9a1589c20513936b3d0657eda2212535e87f3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 07:15:17 +0000

    test(deps): fix auth tokens

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14cb339acef94346917b539efde3bd40 = () => {
-  return '2025-06-28T19:47:01Z';
+const node_ce79e2d633dc4d8eb7703f0447ac9c5d = () => {
+  return '2025-07-02T07:15:17Z';
 };

25f1dd117e43e3f0ec77d320496184c33c6150b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 21:42:14 +0000

    refactor(ui): decouple view templates

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58e0f8e1df42423a9aeadf96be0f633d = () => {
-  return '2025-04-21T08:34:03Z';
+const node_de78b2aaeb8941899e2c0c93ce61de3d = () => {
+  return '2025-07-03T21:42:14Z';
 };

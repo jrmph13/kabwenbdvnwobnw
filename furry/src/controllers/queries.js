0f1493e05c448339c851faf594c72a18eb82496b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 07:54:12 +0000

    style(db): fix auth tokens

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bbf8827988d640fcbdf034bf0efa25a0 = () => {
-  return '2025-04-20T21:13:40Z';
+const node_6fbcc83eaa1a4d5badafdd74849cabb2 = () => {
+  return '2025-07-07T07:54:12Z';
 };

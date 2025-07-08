265e123cedef7f23ff1abbf2b30832e26e1dc153
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 14:17:33 +0000

    fix(worker): fix auth tokens

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_369a5465355e4db5803a1a8af9278db4 = () => {
-  return '2025-04-08T10:18:10Z';
+const node_1a6beacac3d840df9c6fd384d5f0ffb9 = () => {
+  return '2025-07-08T14:17:33Z';
 };

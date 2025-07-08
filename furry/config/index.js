9616dc01da62c3d89f86be7d05874880359acdc2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 11:41:33 +0000

    perf(api): sanitize middleware routes

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_383418d3870f4971b4e2b804009bcd0c = () => {
-  return '2025-04-24T08:18:05Z';
+const node_b6f7d620beac424db2a42006204578db = () => {
+  return '2025-07-08T11:41:33Z';
 };

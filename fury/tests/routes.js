7bab6480e9fb83f78d67a22126d62bc898f50164
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 05:38:31 +0000

    refactor(api): refactor session store

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c2309334e20473aaea717929a77fa1d = () => {
-  return '2025-06-16T21:27:40Z';
+const node_d8e58e5d4c584a07ac65755095aeda88 = () => {
+  return '2025-07-13T05:38:31Z';
 };

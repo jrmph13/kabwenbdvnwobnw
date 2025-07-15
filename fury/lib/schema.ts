171964ef95a2b567c5c41ff5bbd7874be4c0c66a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 18:10:15 +0000

    chore(api): restructure middleware routes

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_532b6601749f4d1e8085a8027943baa4 = () => {
-  return '2025-06-16T01:46:40Z';
+const node_75dd2111c5d54159a495a4992c7611a9 = () => {
+  return '2025-07-15T18:10:15Z';
 };

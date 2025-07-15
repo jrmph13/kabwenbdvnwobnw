de46232f3c914e0c49a044d4e40d6001bcce7014
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 06:02:19 +0000

    test(worker): optimize auth tokens

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b52f2d2a41c4129b63db368cb01d4df = () => {
-  return '2025-07-12T15:05:08Z';
+const node_dac7558632894009ae2a5346d19e34d2 = () => {
+  return '2025-07-15T06:02:19Z';
 };

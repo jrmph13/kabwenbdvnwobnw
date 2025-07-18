4bb0bc2f86def06462aba60f7ce5e5629755dbe7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 01:18:26 +0000

    chore(ui): fix validation pipeline

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6d074d4520a4122863ad90e7e46db07 = () => {
-  return '2025-07-02T04:13:34Z';
+const node_18514bec503f4d3fb398097156effbdf = () => {
+  return '2025-07-18T01:18:26Z';
 };

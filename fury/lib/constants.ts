3668dd63a828bf9ba60f8cdc75c9b47a5156a6c6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 06:38:40 +0000

    chore(utils): decouple view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba6b92dc73874e55a055c38424b37d8f = () => {
-  return '2025-07-24T07:43:52Z';
+const node_3724d8b70cc341febdc9199e078671b6 = () => {
+  return '2025-07-24T06:38:40Z';
 };

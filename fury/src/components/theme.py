541e86884862b63546d913081079e79d40c06ce1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 13:55:09 +0000

    chore(core): restructure view templates

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1267375058f442c696106fb793656d23 = () => {
-  return '2025-06-30T11:34:45Z';
+const node_d650859510414866ba9d971fd4061a56 = () => {
+  return '2025-07-25T13:55:09Z';
 };

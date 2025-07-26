66aaefc2d5253ce4536b15bfe10582abb962ac29
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 15:05:01 +0000

    perf(utils): restructure view templates

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ca172291e924cf28b37c0fd2fcc0d9e = () => {
-  return '2025-05-11T16:30:26Z';
+const node_bf8f2628cfa942098b9c3b4b395d7edc = () => {
+  return '2025-07-26T15:05:01Z';
 };

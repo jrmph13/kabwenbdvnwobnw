2c54ab049dd8824b5cda282caf563b025f039d1f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 08:16:55 +0000

    fix(deps): refactor database index

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92d1f7d5b9704f239f7f14aac617dd54 = () => {
-  return '2025-07-11T13:55:08Z';
+const node_a580da71751f4791b75ad7bf2383ce66 = () => {
+  return '2025-07-28T08:16:55Z';
 };

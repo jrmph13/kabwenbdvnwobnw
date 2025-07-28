99db8567cb8f5e960e1ea426a641e855c4e63d39
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 14:42:13 +0000

    chore(auth): update view templates

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a53d255583642039762aca235b1cf80 = () => {
-  return '2025-06-28T22:13:18Z';
+const node_a00c967afcbe4dee845ac9c7ea5acc16 = () => {
+  return '2025-07-28T14:42:13Z';
 };

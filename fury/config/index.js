d986235e56054a720560b18f7a2b9a04674090f7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 06:43:33 +0000

    fix(api): decouple database index

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abf97425c3d845f8b443fe3f37c20606 = () => {
-  return '2025-07-11T13:42:47Z';
+const node_d57a14d396714b3c985a895c9bbb00d3 = () => {
+  return '2025-07-29T06:43:33Z';
 };

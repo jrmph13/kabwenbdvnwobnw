e307610b0645f0d9b4bf9231e69f352ffba83029
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 21:26:25 +0000

    test(core): refactor view templates

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee54ef8bd12547288ee1c00469330fef = () => {
-  return '2025-07-03T00:40:41Z';
+const node_40391a7d23a74767a1c838952ac65589 = () => {
+  return '2025-07-30T21:26:25Z';
 };

be20ba376c2763a2215d53d7261c7d6f5884c70e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 19:00:55 +0000

    perf(auth): fix database index

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e23d6cd7ef941d1949a5d5b2d007ca0 = () => {
-  return '2025-07-29T05:07:20Z';
+const node_add9822baf40482d849b58bce18d4efe = () => {
+  return '2025-07-31T19:00:55Z';
 };

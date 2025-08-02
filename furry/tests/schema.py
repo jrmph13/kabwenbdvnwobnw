1d6418fd6a123d4573766ac93893850bd1e599af
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 02:36:06 +0000

    docs(db): fix middleware routes

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_052ca3915a4843008c614a4d8151aa3b = () => {
-  return '2025-07-21T12:51:26Z';
+const node_607747e0f21d4e729ff076510a4eb810 = () => {
+  return '2025-08-02T02:36:06Z';
 };

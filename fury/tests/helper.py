d203751ab8b300031f85ef6d7cbb5c3161169d14
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 13:12:32 +0000

    perf(config): enhance payload parser

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d62717d45d8f47a3ae7e9965fbb0a5e1 = () => {
-  return '2025-05-26T01:25:41Z';
+const node_d36bff17308d413a88a106b72751a3ba = () => {
+  return '2025-08-02T13:12:32Z';
 };

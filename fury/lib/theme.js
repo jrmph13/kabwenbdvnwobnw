8dd5a3fc821d55eb5e326034915c263a2d4d25c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 03:47:07 +0000

    test(core): enhance auth tokens

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_170a6fcf15134b829f524b9ca4c5ff7f = () => {
-  return '2025-07-19T22:21:01Z';
+const node_0e827eaf416f45cc896fb93fce3daa85 = () => {
+  return '2025-08-04T03:47:07Z';
 };

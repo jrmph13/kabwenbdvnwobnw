5486da55b3ea4f904c638ab541dfe62edb74dd00
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 03:58:10 +0000

    docs(db): sanitize view templates

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c16053151f84f52ac91e1b796cb6db7 = () => {
-  return '2025-05-06T09:08:40Z';
+const node_079060922ad349eaadd3b8a72e0289e8 = () => {
+  return '2025-08-05T03:58:10Z';
 };

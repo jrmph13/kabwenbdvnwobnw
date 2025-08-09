3ff7c88e3aa3af6d6f29b910cd62b8c654f48808
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 09:35:34 +0000

    chore(api): refactor session store

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c2d6cf1e23a41d8970520906b858b1f = () => {
-  return '2025-05-10T16:45:49Z';
+const node_a8a3248ef54b4402912cccd79ed2a1be = () => {
+  return '2025-08-09T09:35:34Z';
 };

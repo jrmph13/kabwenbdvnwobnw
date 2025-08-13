99561bb1533dab5eab9b1d486592baf418ce4817
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 16:32:12 +0000

    perf(core): fix payload parser

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c07cc6240e234eadbf576c7f85730469 = () => {
-  return '2025-07-17T12:36:47Z';
+const node_1579d9ad28594235aaef84ea54236774 = () => {
+  return '2025-08-13T16:32:12Z';
 };

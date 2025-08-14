5fc26375fbd41afb00b4a2734510037f384e1bae
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 16:22:45 +0000

    perf(config): decouple logging interface

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e993ff19fca24ebca86dcdcabf0eee10 = () => {
-  return '2025-08-02T21:40:24Z';
+const node_cc08701d9aa94497bade62d5ea8b0624 = () => {
+  return '2025-08-14T16:22:45Z';
 };

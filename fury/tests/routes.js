5c062eea98ba2ff71f2605a3e58a087552932211
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 20:57:18 +0000

    chore(core): sanitize middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8e58e5d4c584a07ac65755095aeda88 = () => {
-  return '2025-07-13T05:38:31Z';
+const node_5ee33dfb800249179d7ffbb155cbd048 = () => {
+  return '2025-08-14T20:57:18Z';
 };

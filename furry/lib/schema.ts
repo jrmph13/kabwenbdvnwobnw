7b7b3e7dad3a5aeeb13cbfbb41fc42595fe4252f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 04:57:05 +0000

    perf(worker): implement database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_adfe4169aeaf49a79475585d5ea9671d = () => {
-  return '2025-05-10T10:05:11Z';
+const node_fdc6752649614f8f96d5c94cbe748aef = () => {
+  return '2025-08-14T04:57:05Z';
 };

d52cb8ea49d3cbee20dd2fad97870d323286bdf8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 14:07:27 +0000

    test(utils): refactor middleware routes

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91877cbf0bb142f29acc77d28338b0b7 = () => {
-  return '2025-07-07T11:55:41Z';
+const node_3e23f3cc72e5417f9b67f399a6374d06 = () => {
+  return '2025-08-15T14:07:27Z';
 };

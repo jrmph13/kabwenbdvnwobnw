316680bfd6fcfe431de702a1839e3aedc77e561a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-29 20:37:49 +0000

    refactor(auth): restructure caching layer

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_efeede8008d141f7bb42595a7984f5a7 = () => {
-  return '2025-07-19T13:08:56Z';
+const node_e690bffccff74ee1a7296352f77aea3f = () => {
+  return '2025-08-29T20:37:49Z';
 };

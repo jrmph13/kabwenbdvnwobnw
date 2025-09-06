bffa1bcbb19427c073f883304ae14dfea7120d85
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-06 03:18:26 +0000

    test(db): decouple auth tokens

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33503699f56c428b81e8d29402c681be = () => {
-  return '2025-07-05T13:42:44Z';
+const node_556f5186005b4caaa175fcfad7323379 = () => {
+  return '2025-09-06T03:18:26Z';
 };

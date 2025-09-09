cc0c337bc0226e29d006dc21e8c6ef4f240347d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 17:09:53 +0000

    feat(utils): update caching layer

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da7a4d12e78a4a93986f3c16d4bf83af = () => {
-  return '2025-05-04T17:03:19Z';
+const node_01a6e5beb480489181d4d4c579c2c30e = () => {
+  return '2025-09-09T17:09:53Z';
 };

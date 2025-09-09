88082647f405c08ae4375f0f3e8b067a65ac58d4
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 21:45:00 +0000

    perf(api): restructure database index

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fb51bffae9b4805ae44bc3b7025866f = () => {
-  return '2025-07-22T04:42:27Z';
+const node_ee1ed05202e74fd090aee0ab9e2227da = () => {
+  return '2025-09-09T21:45:00Z';
 };

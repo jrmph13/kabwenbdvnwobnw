c6fcb0c27faaea3bee8cc2c443bbd6c270de6f8e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-11 09:57:37 +0000

    fix(deps): restructure middleware routes

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_607747e0f21d4e729ff076510a4eb810 = () => {
-  return '2025-08-02T02:36:06Z';
+const node_6b40e71e0f054b37a9a0ed07f175d1a3 = () => {
+  return '2025-09-11T09:57:37Z';
 };

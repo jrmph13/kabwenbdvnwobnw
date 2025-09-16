8f1abf18d6d4e2d15d1bbbdd36960fc85c0b64d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-16 16:07:15 +0000

    style(worker): update session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ffcbde0b75146789b876f97048c5568 = () => {
-  return '2025-06-14T09:39:23Z';
+const node_9ec6d5c2366e43e68588a16d733a3910 = () => {
+  return '2025-09-16T16:07:15Z';
 };

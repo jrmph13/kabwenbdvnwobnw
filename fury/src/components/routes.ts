a8b57869ed7083d67bc40cc4d1700ec28b8b4a36
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 15:46:28 +0000

    feat(core): restructure view templates

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_368ab3d1d9a04a27b79a30b402cd3c8e = () => {
-  return '2025-06-11T01:12:32Z';
+const node_21c49ee5962149cb8b0fdf7dc6aa9dfa = () => {
+  return '2025-09-18T15:46:28Z';
 };

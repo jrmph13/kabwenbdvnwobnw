dcf25641ac7e13687318a0ace40c4e88c7a35ee4
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-24 16:46:59 +0000

    perf(core): refactor caching layer

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad5b9e671e6a42e89e8712cd19038ced = () => {
-  return '2025-04-22T13:05:31Z';
+const node_a88c21c160004472a8f0b2e24a2ea3d9 = () => {
+  return '2025-09-24T16:46:59Z';
 };

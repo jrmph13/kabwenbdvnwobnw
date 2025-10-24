1fa6f1602fe3f4c1f9d3491d389217a705a636a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-24 03:47:53 +0000

    style(deps): update middleware routes

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f728cb63cb54449d8a57b19ebf80570e = () => {
-  return '2025-05-25T01:25:59Z';
+const node_6b8ce9fc79094d078b29da09670d6801 = () => {
+  return '2025-10-24T03:47:53Z';
 };

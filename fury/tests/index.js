058813a2fc2891e5704dca72300795f9fcbceb07
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-25 21:34:23 +0000

    feat(api): refactor caching layer

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7bffc0886ba747c4a0decf2031c15a8b = () => {
-  return '2025-07-28T23:19:21Z';
+const node_cd39ae4cf87340658826497ebad66790 = () => {
+  return '2025-10-25T21:34:23Z';
 };

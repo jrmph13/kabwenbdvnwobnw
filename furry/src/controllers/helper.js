28d6d29b9802b70a0a33fb9af7b25bd17c516197
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 06:09:33 +0000

    chore(ui): sanitize middleware routes

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22cbf2f8a1d548809798dbae23aa518b = () => {
-  return '2025-07-20T12:52:43Z';
+const node_bd15d192acae4305b2b383ff1d761258 = () => {
+  return '2025-10-26T06:09:33Z';
 };

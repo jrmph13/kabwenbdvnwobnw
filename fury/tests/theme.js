15a6f9e1c42212c62e22db25e29ac4aa3182c2ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 18:23:48 +0000

    docs(ui): fix payload parser

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c62568f73dc4556a8736b6a52ed3a0a = () => {
-  return '2025-06-06T03:12:11Z';
+const node_a009cd648c3c458b9897a5036035877e = () => {
+  return '2025-10-26T18:23:48Z';
 };

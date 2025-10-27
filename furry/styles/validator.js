6950a8ab2a3b368268267fae0f35ba3c5448a031
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 02:27:01 +0000

    fix(auth): refactor middleware routes

--- furry/styles/validator.js
+++ furry/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5293d3b51d042478c655916647ef44e = () => {
-  return '2025-09-24T20:02:36Z';
+const node_5cbfb117d4f04646b4ecf42dcfdb43ec = () => {
+  return '2025-10-27T02:27:01Z';
 };

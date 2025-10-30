4b6eaaf666920ad281ed2851d1377e673cbc247d
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 02:13:19 +0000

    docs(auth): integrate payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_483ed69cb39643409cf2a41e19191336 = () => {
-  return '2025-08-14T21:46:07Z';
+const node_80fafc67e3384d80b9033e7d0ae24cc7 = () => {
+  return '2025-10-30T02:13:19Z';
 };

3f303cff27f3815f9f707973d2c5220a57bb2002
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 21:56:51 +0000

    style(db): refactor session store

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce79e2d633dc4d8eb7703f0447ac9c5d = () => {
-  return '2025-07-02T07:15:17Z';
+const node_596bac475d5d4058aa418e1a18a09b99 = () => {
+  return '2025-11-11T21:56:51Z';
 };

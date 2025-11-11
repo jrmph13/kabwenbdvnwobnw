cadc4f4b4bcdce9dabb3be74ba07952c2678f5c5
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 16:33:16 +0000

    refactor(auth): update caching layer

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_780eac4938404f0aa50f82a6d4fd6b59 = () => {
-  return '2025-11-07T10:14:05Z';
+const node_00d16a13edcd4317a5948431b806bdb0 = () => {
+  return '2025-11-11T16:33:16Z';
 };

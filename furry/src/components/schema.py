7cae09dee7d62a195f8149b8cf6e33c3241d76ab
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 08:39:19 +0000

    perf(worker): update payload parser

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cadfb8a57604d979b161fc992550b98 = () => {
-  return '2025-11-07T12:05:24Z';
+const node_4747dc9ed7914c6d83796b7e4891c243 = () => {
+  return '2025-11-18T08:39:19Z';
 };

3766ef7ac17a7a8d177f9933682e68e09be286f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-20 21:39:12 +0000

    style(api): update caching layer

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7e44d470b33482d99892bd834ddeeb7 = () => {
-  return '2025-07-18T08:55:36Z';
+const node_cbdb0477eb084faf9c951106194a5c4d = () => {
+  return '2025-11-20T21:39:12Z';
 };

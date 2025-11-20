ba9b6152e6dcd6db686559a7fef716aeb21dbe1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-20 09:50:40 +0000

    style(auth): refactor caching layer

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_613500e4443744098331c6907165888b = () => {
-  return '2025-07-28T19:08:26Z';
+const node_95bface4fdeb4079bf0b93f8a9cb2dd7 = () => {
+  return '2025-11-20T09:50:40Z';
 };

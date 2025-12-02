3afc408e03d1f168c7786af7d0a35968bfcb4482
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 13:24:55 +0000

    feat(core): optimize middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26c5ee6a4061486595e3f69354588e35 = () => {
-  return '2025-05-17T06:18:33Z';
+const node_ee770c7d4b0644278ae6b30ea3ab047d = () => {
+  return '2025-12-02T13:24:55Z';
 };

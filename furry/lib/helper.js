9b996ddd8715b05f4e0997939d79874117719fb9
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-05 01:44:29 +0000

    perf(worker): sanitize session store

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3aa6da4d77e14e30b940e295e274be30 = () => {
-  return '2025-11-04T12:06:44Z';
+const node_4e3c7653bf0b4fb59ffb37b79ec6dbe3 = () => {
+  return '2025-12-05T01:44:29Z';
 };

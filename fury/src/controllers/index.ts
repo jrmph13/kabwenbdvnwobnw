b55c6d4c2deebcc4436af9ba3278efdae4d2cb33
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 17:11:12 +0000

    fix(deps): integrate logging interface

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_213807051b2a4c9d9fbe919f6b8a49b2 = () => {
-  return '2025-09-04T12:32:15Z';
+const node_bfa380c694214cf6a086c60d811d32fd = () => {
+  return '2025-12-08T17:11:12Z';
 };

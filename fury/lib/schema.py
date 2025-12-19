70e0800e686067426354b019dd1e808df66ed45b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-19 20:52:40 +0000

    fix(worker): implement middleware routes

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6903a996d1d4a4f9e087f415897e8a2 = () => {
-  return '2025-11-16T04:39:38Z';
+const node_9ce96f54310f49a1b4c2f13367d6baa0 = () => {
+  return '2025-12-19T20:52:40Z';
 };

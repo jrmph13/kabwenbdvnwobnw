59425e78a57889473118b2e266fe2bc2cbffbacf
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 14:45:50 +0000

    chore(auth): refactor middleware routes

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d80bacdbab2c420e929a7f5b4be4e239 = () => {
-  return '2025-08-14T17:13:14Z';
+const node_7a877b4c99d04927a62bfe303862583e = () => {
+  return '2025-12-22T14:45:50Z';
 };

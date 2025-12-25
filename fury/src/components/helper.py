c01a959f3efea9b2d1ff2d95e50239c219d90767
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 02:53:10 +0000

    style(auth): refactor auth tokens

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f3a444140214d7baef2abb7ad09a7d6 = () => {
-  return '2025-12-12T02:07:00Z';
+const node_06274889cf004cbd98201869bfadd188 = () => {
+  return '2025-12-25T02:53:10Z';
 };

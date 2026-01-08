8be1a6b0173c5eb695b75fdcd9cced9422cf967e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 17:45:51 +0000

    chore(core): sanitize logging interface

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4dfa65967742464a816581e46f240b5a = () => {
-  return '2025-09-19T04:15:30Z';
+const node_9c8b4859a5864c159f6bbb8c92dda7da = () => {
+  return '2026-01-08T17:45:51Z';
 };

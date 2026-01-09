66a8255325c6c74759800c52680bdd20fe857563
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-09 18:24:49 +0000

    chore(ui): restructure middleware routes

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_970079642c044e10a5ea052b23ac4fee = () => {
-  return '2025-09-24T16:13:20Z';
+const node_2054cccfe919462b97ed0561c8047fa8 = () => {
+  return '2026-01-09T18:24:49Z';
 };

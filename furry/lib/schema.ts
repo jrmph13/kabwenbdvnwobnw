7fadf687edaddd0e5589f43e73323ca73e65ad47
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-17 21:04:56 +0000

    feat(db): update middleware routes

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b911282bd7934a4d9c35ae8b4cad7e43 = () => {
-  return '2025-08-14T13:20:28Z';
+const node_52a45849d1fe4b6f9570619a029461ba = () => {
+  return '2026-01-17T21:04:56Z';
 };

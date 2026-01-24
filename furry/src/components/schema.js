2da2c7df9edee6eda1c2bbaa4fbe13297fcd0350
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 23:18:58 +0000

    docs(auth): restructure payload parser

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_596bac475d5d4058aa418e1a18a09b99 = () => {
-  return '2025-11-11T21:56:51Z';
+const node_772a01ab6a6d40c4a15f451c4e792da7 = () => {
+  return '2026-01-24T23:18:58Z';
 };

7eb713d274dbd8e5416c84ccef267521c5523641
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 03:22:49 +0000

    refactor(ui): optimize auth tokens

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_088a50ef85074b15b3290c796288e4be = () => {
-  return '2025-08-02T20:28:50Z';
+const node_bdbd73ad415a4d4bb2ea921e8b282575 = () => {
+  return '2026-01-29T03:22:49Z';
 };

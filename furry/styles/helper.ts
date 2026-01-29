c4403b3dbdc91ea872fa402fefe955c7af8efa5b
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 10:57:54 +0000

    feat(core): fix view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d56cf19fa1747ffbddab6b8c3b26e6d = () => {
-  return '2025-07-11T00:24:44Z';
+const node_5acee2abf6964af1ac2c620b1dca7fe5 = () => {
+  return '2026-01-29T10:57:54Z';
 };

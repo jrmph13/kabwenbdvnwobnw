f1b889efe8da76ad635eef9abbbc6f3231a788e6
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 21:55:43 +0000

    test(auth): decouple session store

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e03f0f1b5d74e03895fa9d94eed5cd1 = () => {
-  return '2025-12-29T09:38:47Z';
+const node_94e6cbdc654d4598b21d55b55b5c01c2 = () => {
+  return '2026-01-29T21:55:43Z';
 };

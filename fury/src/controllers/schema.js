62ba5d9d61b6ce125badb3281428035e8925113f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 05:27:23 +0000

    fix(utils): restructure view templates

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38bbe7415e834baf95514fdeecf66c93 = () => {
-  return '2025-06-22T19:07:27Z';
+const node_e3fed7f5109d405d98873519df4f1cea = () => {
+  return '2026-01-30T05:27:23Z';
 };

07f19fdbff2d047fb7ef3e1839702724944387b9
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 09:46:12 +0000

    fix(api): refactor view templates

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4587e6d166b44753b6a3eb7db3bfba02 = () => {
-  return '2025-12-09T01:50:18Z';
+const node_ebed971560b942c1bea58fa18bbac63c = () => {
+  return '2026-02-02T09:46:12Z';
 };

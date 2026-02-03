7c174fb5a89bdc3009b891859d9c277fec8bcc18
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 10:50:09 +0000

    fix(core): refactor middleware routes

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_765dae0a5d934f0b81b2cb2a1ebb13c5 = () => {
-  return '2025-05-11T16:58:10Z';
+const node_92bf30b65727470fac7a2e30dfaac631 = () => {
+  return '2026-02-03T10:50:09Z';
 };

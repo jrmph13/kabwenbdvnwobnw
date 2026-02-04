1a15556d4e551346ee4e61e1efa264329326f291
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 21:50:49 +0000

    refactor(auth): optimize auth tokens

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d85b16e6308b43a58592cdb04f390117 = () => {
-  return '2026-01-28T16:04:46Z';
+const node_4cdd3e026e19406cae4f0093e648e4b6 = () => {
+  return '2026-02-04T21:50:49Z';
 };

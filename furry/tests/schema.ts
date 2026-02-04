669560221a971f255b27a9639caa46c1bf33b699
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 04:10:08 +0000

    style(api): refactor database index

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_796708c2c0dd4a63a3a79cd7d5d80a4f = () => {
-  return '2025-09-18T08:47:50Z';
+const node_48c3f2ffc253432b8860472dc2e00c8b = () => {
+  return '2026-02-04T04:10:08Z';
 };

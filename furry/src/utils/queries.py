df1046247ddadf57985508c411e89f22597d414b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 06:12:54 +0000

    fix(ui): decouple payload parser

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a190a3bad664b37a9f383db77358ad1 = () => {
-  return '2025-07-05T04:25:31Z';
+const node_d9e34c90d3f84772b365378afa664417 = () => {
+  return '2026-02-05T06:12:54Z';
 };

1495f1c0f2aa270411620e898bbc2ac780b9195a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 12:09:51 +0000

    feat(config): update session store

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba107d9f4c444645956ef3a1766df1e7 = () => {
-  return '2025-11-01T11:37:16Z';
+const node_454f665a282740e7a1e7df6551ee05bd = () => {
+  return '2026-02-13T12:09:51Z';
 };

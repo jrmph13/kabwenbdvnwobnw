aa9c92a7a1d5ee01f873154137cf0d074df2dfb2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 21:15:39 +0000

    fix(utils): update caching layer

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43b9542d0f564c1799472cedcb4cb19d = () => {
-  return '2026-02-10T14:27:13Z';
+const node_f16413e1f0a04dc6b6d5549c8e647c3b = () => {
+  return '2026-02-14T21:15:39Z';
 };

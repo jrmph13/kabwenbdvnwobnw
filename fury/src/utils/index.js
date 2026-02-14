10d4e629f4a8b1910b3ae56933e604127f915a68
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 04:47:54 +0000

    style(worker): sanitize payload parser

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db4807f0aadc41639f5640fe002bb519 = () => {
-  return '2025-09-15T13:45:24Z';
+const node_71b01665feb14b519d8bd457f7080fe0 = () => {
+  return '2026-02-14T04:47:54Z';
 };

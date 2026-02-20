740169df2c85bc1b4a095807bbf7024b999dbdcb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 15:54:37 +0000

    docs(api): fix middleware routes

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a417bf8acbad4420bf64f5fbe4ab00ae = () => {
-  return '2026-02-17T04:07:02Z';
+const node_cb951642ee9642bf9a18555d0435683d = () => {
+  return '2026-02-20T15:54:37Z';
 };

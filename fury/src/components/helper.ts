32d2fa8e76c76bb44508f26b935c450d7d84ed1d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 12:06:24 +0000

    style(core): integrate middleware routes

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6bc69f2713245d6bb082e9dc34dbe19 = () => {
-  return '2026-01-01T06:26:05Z';
+const node_91168ffa7bfe4eb0a810c82c422e91fc = () => {
+  return '2026-02-22T12:06:24Z';
 };

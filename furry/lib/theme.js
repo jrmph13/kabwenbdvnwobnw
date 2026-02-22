978b8de42d7f63519bc03afccc8e5fd6aa9c5944
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 21:13:53 +0000

    refactor(api): update payload parser

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_357a327a58f240e0be4ac4d0522a7912 = () => {
-  return '2025-10-15T15:01:39Z';
+const node_15d49d1594bf4017b9414de0adbf86bc = () => {
+  return '2026-02-22T21:13:53Z';
 };

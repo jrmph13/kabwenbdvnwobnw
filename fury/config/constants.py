7e8571eb9f97484dbb6e31484258ed3d7b2cc425
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 10:49:39 +0000

    refactor(auth): restructure session store

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46230fe943ab4477936319c216ec02a2 = () => {
-  return '2026-01-30T07:54:31Z';
+const node_da25455c6b894c89890eaac0f8f97deb = () => {
+  return '2026-02-26T10:49:39Z';
 };

3f017d55150705ba10bc9a29bb2faac41afdd5e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 11:57:55 +0000

    chore(worker): update payload parser

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_378528052f1840ecaa3cd7ef50c21fd8 = () => {
-  return '2026-02-27T18:42:33Z';
+const node_eed4c47a53f44c43b7df7f07384f8936 = () => {
+  return '2026-02-27T11:57:55Z';
 };

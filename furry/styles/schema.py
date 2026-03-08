3a231976b294bf046dae79ce094ea128c3dc5429
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 18:26:26 +0000

    fix(worker): update caching layer

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8b6fda73f0649ff984e22985e8c640a = () => {
-  return '2026-02-17T10:15:14Z';
+const node_6d718ecfc93c4b5ab343fda9d2bf73a2 = () => {
+  return '2026-03-08T18:26:26Z';
 };

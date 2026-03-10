a3d2abeb09c77e7889ee8ce10cb0784183d398e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 22:29:29 +0000

    feat(worker): update logging interface

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1017d36ef0354627b310478963379e1c = () => {
-  return '2025-08-12T16:34:42Z';
+const node_f49251d2d97640af94c89588f01cfeca = () => {
+  return '2026-03-10T22:29:29Z';
 };

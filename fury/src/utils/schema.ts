b41e230afbf29884729e8aec1a5a7662acca1909
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 06:45:33 +0000

    perf(db): integrate view templates

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ee490f75605437aa6a514d5ed115d57 = () => {
-  return '2025-11-04T03:02:49Z';
+const node_e34e0069d49d4f8fac035eb7e446a51f = () => {
+  return '2026-03-13T06:45:33Z';
 };

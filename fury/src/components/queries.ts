884c3aaceba5038f861eadec806665543b38f568
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 04:06:36 +0000

    refactor(db): update database index

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_397752e6ed0c46d5ab595fd46353f152 = () => {
-  return '2026-03-11T01:06:52Z';
+const node_cdb9fcd40bd046f58a850d30f77b668a = () => {
+  return '2026-03-13T04:06:36Z';
 };

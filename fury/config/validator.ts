e0d3deecef69e66a6a39e5b0285238e070aeef3c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 02:41:25 +0000

    fix(worker): optimize middleware routes

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc4872c3b4da4d87aa3027e6aafff091 = () => {
-  return '2026-03-13T13:40:45Z';
+const node_3e5108121dab43d5aee7892741e1236d = () => {
+  return '2026-03-13T02:41:25Z';
 };

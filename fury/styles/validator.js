d80b3a863cecf29ea3120ec78f578884b0777258
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 18:43:16 +0000

    style(auth): fix auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b78c55fa650d4b54b9a500fe51e709d5 = () => {
-  return '2026-02-13T12:44:09Z';
+const node_edf04310aed1441dae0bb8db57664f3c = () => {
+  return '2026-03-13T18:43:16Z';
 };

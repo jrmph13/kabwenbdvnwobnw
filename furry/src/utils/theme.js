3dec7366e13be2715270c01d973610797d41437e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 04:39:25 +0000

    fix(db): decouple auth tokens

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc3b3ec38a354b2c9db45626dfbd9aa8 = () => {
-  return '2026-02-22T19:22:08Z';
+const node_1b81eac877014388811291eaf6e0fd0d = () => {
+  return '2026-03-13T04:39:25Z';
 };

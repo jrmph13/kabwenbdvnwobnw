29082d4ff628529da28bd9d6d12e62bf7ba337b1
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 11:31:17 +0000

    perf(db): refactor session store

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a603a35723cd4e1880e17fb57f1c4ecb = () => {
-  return '2025-07-05T08:18:23Z';
+const node_c18975d65b074436bdcb5c3f79a45c11 = () => {
+  return '2026-03-14T11:31:17Z';
 };

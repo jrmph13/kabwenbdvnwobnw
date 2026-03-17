a3baccb8f50dceb6ce12b8eaf225009b2090e343
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 15:43:04 +0000

    chore(deps): refactor view templates

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ab945d5d29049a883ad09362ed19c09 = () => {
-  return '2026-02-26T23:03:19Z';
+const node_49fe1ec3757b471eb26dddc94a8cb8d0 = () => {
+  return '2026-03-17T15:43:04Z';
 };

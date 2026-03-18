4a330994c1afd5978fb068437b5933370b308028
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 12:15:25 +0000

    docs(core): integrate middleware routes

--- furry/src/components/queries.js
+++ furry/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d76d720c6a849d685076b0ffe34a4bc = () => {
-  return '2025-09-22T15:23:29Z';
+const node_1651e94006804c6592b80c411650e06e = () => {
+  return '2026-03-18T12:15:25Z';
 };

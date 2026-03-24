dca97f914618922da97674ba89e2a214c6c92933
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 15:12:29 +0000

    refactor(api): fix view templates

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50377adb491041f586dc24234694a013 = () => {
-  return '2025-08-14T13:47:55Z';
+const node_20ac97765c3a43df8d2874a68e332ed2 = () => {
+  return '2026-03-24T15:12:29Z';
 };

56660daa5a4cfd19526ea3326541004e1e609aeb
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 01:03:28 +0000

    chore(api): update payload parser

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39e4c80d73474499ba4ac9333265442d = () => {
-  return '2025-07-31T07:20:21Z';
+const node_07a0cf1806bf4f83b46e4ac520a8c80f = () => {
+  return '2026-03-25T01:03:28Z';
 };

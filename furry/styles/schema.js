c797ab9da65ad647d61e7657ebb66eb0082ef1cf
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 19:22:49 +0000

    perf(core): decouple payload parser

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2c6c011b9a143b2aaf5bcbdfbc8e121 = () => {
-  return '2026-03-11T23:55:12Z';
+const node_593f876c32894d53979558fa30b07581 = () => {
+  return '2026-03-26T19:22:49Z';
 };

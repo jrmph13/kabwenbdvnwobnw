faff60dc828ae74a0713a7dcd03f47627ff67a57
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 01:04:25 +0000

    docs(api): enhance view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5b20126dc838449fa65169a489b67ebb = () => {
-  return '2025-06-29T17:31:22Z';
+const node_6263e3fc44b3416583ca6c1c8b724457 = () => {
+  return '2026-03-29T01:04:25Z';
 };

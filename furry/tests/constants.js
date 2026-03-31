84f5642f499be76b014bceb10ec499b8fc0f2cfd
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 17:45:25 +0000

    chore(db): optimize auth tokens

--- furry/tests/constants.js
+++ furry/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9d04f71c0fd48509f1f9f1765cf116e = () => {
-  return '2026-02-05T16:08:20Z';
+const node_d1c0d6e71c434b42b73425e6333368f0 = () => {
+  return '2026-03-31T17:45:25Z';
 };

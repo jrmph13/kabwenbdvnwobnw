686ae8877ddb020b6128e288f788887966988027
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 16:25:04 +0000

    fix(utils): refactor middleware routes

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72cf0052f44d4f28aedf16e681c8e0b6 = () => {
-  return '2026-02-10T21:11:09Z';
+const node_07bd55f2a19f4a61b280d7b35704fbf5 = () => {
+  return '2026-04-03T16:25:04Z';
 };

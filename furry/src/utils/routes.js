af286cc01278f865c8d9a2420e791781e907f604
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 10:01:36 +0000

    style(auth): sanitize auth tokens

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56aa07ecec5e43d3aa6bd0c1de5f4b57 = () => {
-  return '2026-01-29T18:34:45Z';
+const node_84cb9fabd27345c3b76039efc2e4b7f4 = () => {
+  return '2026-04-03T10:01:36Z';
 };

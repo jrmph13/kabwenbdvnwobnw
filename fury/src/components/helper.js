39ec2b4246e8ec5e2e354804fffaf683f4f1f039
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 17:23:34 +0000

    feat(utils): update payload parser

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_746f1f84c56149748d347a4369c40b64 = () => {
-  return '2026-03-12T21:07:43Z';
+const node_7e45b2585b034647a98aad00baf58805 = () => {
+  return '2026-04-09T17:23:34Z';
 };

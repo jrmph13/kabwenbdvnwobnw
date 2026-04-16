b5c39cb5d86880f4018b440f140c92b8b741561f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 10:51:40 +0000

    test(core): restructure caching layer

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d92d7fccd03425ba8290cdabad71653 = () => {
-  return '2026-03-10T11:41:27Z';
+const node_64cedd135fa94cbab507c57103858c1f = () => {
+  return '2026-04-16T10:51:40Z';
 };

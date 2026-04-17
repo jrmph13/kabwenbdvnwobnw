783aed7d54056280d547f18d55e261d2da066997
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 16:09:21 +0000

    fix(deps): integrate auth tokens

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cdd3e026e19406cae4f0093e648e4b6 = () => {
-  return '2026-02-04T21:50:49Z';
+const node_ca487c2fbc2848ee9868637b776d69df = () => {
+  return '2026-04-17T16:09:21Z';
 };

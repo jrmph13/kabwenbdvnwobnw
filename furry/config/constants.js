b16f7ed2f8f2a574d72a004c4b6877504121a0be
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 18:32:14 +0000

    fix(worker): decouple view templates

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3c1e44c8573434a81541c0f0b72ced6 = () => {
-  return '2025-05-21T11:41:53Z';
+const node_ff6d88de05ed4e6794106a359452d1eb = () => {
+  return '2026-04-21T18:32:14Z';
 };

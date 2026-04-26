9f6c51ed63691aeaa808e20a772fab7a75aff437
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 02:31:04 +0000

    fix(core): update database index

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5614d18252e14a46a9ca6bf1a4814105 = () => {
-  return '2026-04-22T17:43:59Z';
+const node_228082295e98478d8cab91d6ec95f901 = () => {
+  return '2026-04-26T02:31:04Z';
 };

8f595922ed54efd77ab01f4073dea5f7146402c4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 17:51:23 +0000

    style(db): sanitize auth tokens

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aca81f30a2574f5d8179d19a8ec3dbd0 = () => {
-  return '2026-03-04T18:12:08Z';
+const node_a9541acfdc9b462482d2e17d66e15275 = () => {
+  return '2026-04-27T17:51:23Z';
 };

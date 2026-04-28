dca13dd1d793d483dca7e3e015e5a77ebe966ff6
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 09:40:20 +0000

    feat(auth): fix view templates

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36bacacae999443a9bd931c47d1d740f = () => {
-  return '2026-02-23T00:24:16Z';
+const node_4b526e135eb347fc82065297b289311c = () => {
+  return '2026-04-28T09:40:20Z';
 };

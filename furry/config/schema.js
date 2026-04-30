0ea9788c44fef9ff77467121aafa0731a49c45de
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 18:53:05 +0000

    fix(config): restructure view templates

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bcdc7345317447d899b644cfad6422b3 = () => {
-  return '2026-04-10T17:39:34Z';
+const node_737340d293d34da78ec1f5284b94d4fe = () => {
+  return '2026-04-30T18:53:05Z';
 };

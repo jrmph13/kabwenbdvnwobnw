1caac34e61e7055372962320d49afb176ca334f4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 16:15:04 +0000

    docs(auth): fix auth tokens

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afeb6ab0e6e74ccb95f871520d13dc96 = () => {
-  return '2025-10-16T09:05:51Z';
+const node_5fcfe4f2bfaf449ea82873c7721abcf7 = () => {
+  return '2026-05-02T16:15:04Z';
 };

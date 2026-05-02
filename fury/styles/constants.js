0bc15a471a10cf8c83911ec30bfdf4feb0c89dcd
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 17:39:42 +0000

    feat(worker): decouple caching layer

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ba4ebf20a7d486f9ab2b39828051ff6 = () => {
-  return '2026-02-27T06:31:09Z';
+const node_600c20c768b84c7b826a4f153c915c15 = () => {
+  return '2026-05-02T17:39:42Z';
 };

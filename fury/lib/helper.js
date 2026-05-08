5d697ca4804526c2cdfb7385af8b0323dc20fd1a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 13:46:20 +0000

    refactor(deps): update logging interface

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f36a7a062c664a8c9988f6bfe3e1afd9 = () => {
-  return '2026-02-17T10:29:41Z';
+const node_bbd9ac4022ac4ab39bb0c1eecf5c5c01 = () => {
+  return '2026-05-08T13:46:20Z';
 };

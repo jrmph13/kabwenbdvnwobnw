926b64b9549039fff057ad74cee4304255ec5b94
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 13:41:59 +0000

    fix(db): fix auth tokens

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dc46e7ab07f4cb6b3dc48c7810f4ed6 = () => {
-  return '2026-03-15T01:51:53Z';
+const node_ba864c3c37574415bc0db2771aa3dcf9 = () => {
+  return '2026-05-10T13:41:59Z';
 };

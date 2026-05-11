7cff141abab35dbcd309b9a5ebadddea5c21090c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 21:20:16 +0000

    feat(core): restructure caching layer

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4198850ed534a039849387408e0bf00 = () => {
-  return '2026-03-04T22:31:56Z';
+const node_8d391077ff0f44168ddff3c79bab7886 = () => {
+  return '2026-05-11T21:20:16Z';
 };

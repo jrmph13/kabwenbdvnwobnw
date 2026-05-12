91a03d319ed76a59aac7f0c2827fbfd19a892cf7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 07:51:05 +0000

    chore(worker): decouple view templates

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97adcce58f1e446c8b9c2ff8d66cabba = () => {
-  return '2026-04-22T12:33:34Z';
+const node_92362a978b9345079e58cb5680910c76 = () => {
+  return '2026-05-12T07:51:05Z';
 };

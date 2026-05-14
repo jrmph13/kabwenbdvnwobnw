19bb1df79817b7a51c78efd0ba8113075101e153
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 21:23:11 +0000

    test(utils): implement auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_600c20c768b84c7b826a4f153c915c15 = () => {
-  return '2026-05-02T17:39:42Z';
+const node_e7ebbe1b126c421cb52af64e10b671a7 = () => {
+  return '2026-05-14T21:23:11Z';
 };

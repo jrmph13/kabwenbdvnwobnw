a485ebe3c35b1374f093c1963e8e43048cb0bd44
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 23:07:23 +0000

    refactor(ui): fix auth tokens

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f7244c999234e7387c6400b89325ce3 = () => {
-  return '2026-04-23T05:47:54Z';
+const node_aaba5faf954c46abad6a991f9599e520 = () => {
+  return '2026-05-14T23:07:23Z';
 };

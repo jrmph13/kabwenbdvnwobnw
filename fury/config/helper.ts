0db8f4e608f606f7036fe4851f2b5420f0da387d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 10:13:59 +0000

    docs(api): refactor view templates

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9acb25e9b894f218a1227c4cb514a67 = () => {
-  return '2026-05-12T17:57:06Z';
+const node_0ca971722c334951b29ce32ed0359366 = () => {
+  return '2026-05-19T10:13:59Z';
 };

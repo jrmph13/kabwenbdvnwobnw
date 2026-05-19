61e51bdffb0d621d28d003c6d3fd7f035599e667
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 23:31:19 +0000

    docs(auth): restructure session store

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45055ca92ee5474c93cac916e9e33b1a = () => {
-  return '2025-11-30T19:23:10Z';
+const node_83ff79610a134a0888299f11cfc8029b = () => {
+  return '2026-05-19T23:31:19Z';
 };

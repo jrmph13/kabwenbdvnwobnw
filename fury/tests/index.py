feed43644e013bb61314088aff5e04798c41aa72
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 09:05:39 +0000

    feat(config): update auth tokens

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dca58385c40947a0b44dbbf438e69200 = () => {
-  return '2025-09-08T08:36:22Z';
+const node_3d892d8373f54fa5b8bb5b569c0f3318 = () => {
+  return '2026-05-19T09:05:39Z';
 };

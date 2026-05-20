6281feffa3c0f8d6062d6a47c682dbf249f927e9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 14:32:08 +0000

    perf(utils): optimize session store

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25915a1d6f1e4f208a9419e70c36e3d6 = () => {
-  return '2026-05-02T08:03:31Z';
+const node_9e19c07a264945a19f8484b5b18e3aa4 = () => {
+  return '2026-05-20T14:32:08Z';
 };

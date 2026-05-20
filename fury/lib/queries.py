9fb6a2723ff4add97c160c9be39a0a403f96a87f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 23:25:29 +0000

    perf(auth): optimize middleware routes

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b2fed3aaf7c4f72ba0d9f5b7aa4fa5e = () => {
-  return '2026-02-22T06:41:17Z';
+const node_4b39e4a0966a40bd8fe6abe45fc10394 = () => {
+  return '2026-05-20T23:25:29Z';
 };

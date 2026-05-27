efe78494f3b47f14c9d51c4b9cb37345ce81853b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 22:41:47 +0000

    chore(worker): refactor auth tokens

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d41c1eb8ba04e539a5ed5f1cba7f05c = () => {
-  return '2026-04-22T15:54:35Z';
+const node_1eee9a134f434f60ac248c682bbb56ce = () => {
+  return '2026-05-27T22:41:47Z';
 };

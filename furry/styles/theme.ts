875b1b29e7eda4d095101549152bbbec5190e4ec
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 17:49:26 +0000

    chore(worker): update view templates

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22097cf0f3894e5aa2c75a5a73d2e372 = () => {
-  return '2026-05-20T03:08:01Z';
+const node_c96bec2460de498ab47cef1df909b83f = () => {
+  return '2026-05-28T17:49:26Z';
 };

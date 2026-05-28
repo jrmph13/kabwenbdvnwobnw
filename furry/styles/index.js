1af56a14eee38f53f01393e3ce3d47894717b4ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 04:39:28 +0000

    test(deps): fix payload parser

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49368b98bcb64ee98cf557c54b705ef2 = () => {
-  return '2026-03-24T10:33:01Z';
+const node_39bbc68b0bd04c2d8ebe2216a7132dac = () => {
+  return '2026-05-28T04:39:28Z';
 };

4372c086b1769c3aa15999c3c7a8c28c7202f9f2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 14:24:46 +0000

    fix(deps): sanitize auth tokens

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60e798679a9242b6b1cbdeeb5f93ee4f = () => {
-  return '2026-03-08T10:34:32Z';
+const node_6496f467fe6f451c824c95521c64f532 = () => {
+  return '2026-05-28T14:24:46Z';
 };

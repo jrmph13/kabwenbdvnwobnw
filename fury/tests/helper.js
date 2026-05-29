4a45a77467be6ab0592c1cabdaaa3a413ecd73d3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 11:06:54 +0000

    refactor(api): update auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58e81914f34c47beb98e75205a12d1d4 = () => {
-  return '2026-04-15T16:39:06Z';
+const node_894d2fc7742a48d7b364bde246f48b23 = () => {
+  return '2026-05-29T11:06:54Z';
 };

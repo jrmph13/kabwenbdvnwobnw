36319bfaa194011a957aef3fefa0788f4cd1a789
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 21:09:46 +0000

    style(deps): decouple auth tokens

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44cf33de45b1452fa2dabb7b69d6e00e = () => {
-  return '2026-05-11T16:58:50Z';
+const node_7a9ac26bd4264b1ea7600f2782ffd2ad = () => {
+  return '2026-06-01T21:09:46Z';
 };

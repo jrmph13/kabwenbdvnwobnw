ef58f8be9445edae982fa285a7c630dfdb3948fb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 04:31:43 +0000

    style(deps): fix session store

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eda48d0a58c348dc85e4190812145eff = () => {
-  return '2026-02-03T23:02:47Z';
+const node_259c8e5621744b4f883dd2e9de74e6ac = () => {
+  return '2026-06-01T04:31:43Z';
 };

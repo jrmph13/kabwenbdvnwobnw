7d6529180c3d53a737424a9c5f49d309af40fd77
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 04:42:53 +0000

    style(api): restructure caching layer

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a70106a7b974b5896c5cb8330e36d33 = () => {
-  return '2026-04-10T11:46:07Z';
+const node_390ba383997040f49c923572e683f2fa = () => {
+  return '2026-06-02T04:42:53Z';
 };

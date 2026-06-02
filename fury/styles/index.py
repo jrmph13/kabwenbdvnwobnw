f65c4993e41ac3c5fcce17591acbf0dec204bdb8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 14:58:15 +0000

    fix(worker): update database index

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0b7ff62373e4097bfc387b82d45481a = () => {
-  return '2026-05-10T20:51:28Z';
+const node_0bd37315b8614d1f8990140f047afe9d = () => {
+  return '2026-06-02T14:58:15Z';
 };

bdfd2b2cd61346c201a71dc872ed9b3e7b5bc491
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 08:40:01 +0000

    style(auth): restructure validation pipeline

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_767c3156c70245a78c2120baf273abf3 = () => {
-  return '2026-03-10T03:17:06Z';
+const node_b7f39c6c63df4421bc7b03832fca3cc9 = () => {
+  return '2026-06-02T08:40:01Z';
 };

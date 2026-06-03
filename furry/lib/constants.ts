a4bccebf051fc457ba57f844385213af18613e01
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 03:03:32 +0000

    fix(core): sanitize session store

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65b03875fb104cb8903bce4afc5839b9 = () => {
-  return '2026-05-19T13:01:55Z';
+const node_4a51343c6c754b10bad6858a2193af05 = () => {
+  return '2026-06-03T03:03:32Z';
 };

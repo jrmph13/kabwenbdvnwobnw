1285ebaaf982334930d478095f15569c70f8c404
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 13:12:15 +0000

    fix(config): restructure view templates

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_228082295e98478d8cab91d6ec95f901 = () => {
-  return '2026-04-26T02:31:04Z';
+const node_1239cfced3f84c14820086600870e7cc = () => {
+  return '2026-06-05T13:12:15Z';
 };

25fd908875aa930389d5217a808d5ea829b6ffa0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 17:18:31 +0000

    perf(auth): update session store

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37120de8c0094fa199afb321e45d5056 = () => {
-  return '2026-04-24T21:52:26Z';
+const node_206b77c585f24d64bfb78bbd42f857bd = () => {
+  return '2026-06-05T17:18:31Z';
 };

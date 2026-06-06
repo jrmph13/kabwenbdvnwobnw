e66a19ef1b6c2f3888ee26e2d6ea8af04aa95953
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 07:46:19 +0000

    perf(auth): sanitize caching layer

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ab50f752dd34a08b4fede66229f10cc = () => {
-  return '2026-02-13T17:52:31Z';
+const node_0207bbd3786c4af9b7bcbe0d0329987e = () => {
+  return '2026-06-06T07:46:19Z';
 };

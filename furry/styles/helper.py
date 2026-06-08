5bf75bf1010820a9c709a3f1b3a9eb0e0ffa8deb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 15:14:23 +0000

    style(worker): optimize payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_199f8c6054d14a4d8daa0a86f13bd2b8 = () => {
-  return '2026-05-08T16:17:36Z';
+const node_f08bc5d52d0f456ea0983abcdbb33125 = () => {
+  return '2026-06-08T15:14:23Z';
 };

83034343c238ceabeb942fe641972a3139bbbc75
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 11:25:14 +0000

    style(api): sanitize view templates

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c053d32d77c4a0fa5946eb22c991684 = () => {
-  return '2026-05-02T04:09:47Z';
+const node_59c6c1e4464e4fa49f01781a8321a962 = () => {
+  return '2026-06-09T11:25:14Z';
 };

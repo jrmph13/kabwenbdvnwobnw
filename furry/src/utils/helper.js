74b757b7f71e069d945303021f1c4b33d77dd2de
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 15:58:56 +0000

    style(api): implement auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dafc0ff0085a417dbb43d8b708f6256d = () => {
-  return '2026-05-19T03:21:03Z';
+const node_7154d9eaf0af41e9b0989aed681aabfa = () => {
+  return '2026-06-09T15:58:56Z';
 };

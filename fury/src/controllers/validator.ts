bf306e734a35ff5684c5842f1bf37a37afed736d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 04:11:53 +0000

    perf(auth): sanitize payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d21ccc497a7341148f3b537db0e15065 = () => {
-  return '2026-05-21T07:23:38Z';
+const node_ba759c95d4484c9085018ceb87aeaa73 = () => {
+  return '2026-06-10T04:11:53Z';
 };

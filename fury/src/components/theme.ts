1c9c2950050a62997bb1b16fc3a72777db2cdda0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 10:03:27 +0000

    feat(ui): decouple view templates

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c985f971bab24516b5526c95d78f0d45 = () => {
-  return '2026-04-16T16:40:03Z';
+const node_d03949509d354144ada7272b4f5e78d1 = () => {
+  return '2026-06-10T10:03:27Z';
 };

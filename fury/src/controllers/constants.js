9c2670aa37329917c8b23647e72e8f74e3f3e5a0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 04:21:41 +0000

    refactor(auth): sanitize payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7266f9fc5af4eafbb54db8b0c106492 = () => {
-  return '2025-11-12T12:38:15Z';
+const node_d952a93a248a46999438de857ad8adb4 = () => {
+  return '2026-06-11T04:21:41Z';
 };

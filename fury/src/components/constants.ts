6e9da453358cf2216e6ba2e616631d14f1561dd4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 18:55:07 +0000

    fix(config): restructure validation pipeline

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f04aafda51434066aaf8dd40d831ac93 = () => {
-  return '2026-06-02T12:44:25Z';
+const node_68e04042a86546568d6f5c58e25e7a61 = () => {
+  return '2026-06-12T18:55:07Z';
 };

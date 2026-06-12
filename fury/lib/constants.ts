97991230208ac1c76b87dff9f5ac08b19d52c6dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 22:36:00 +0000

    feat(utils): refactor session store

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_653fe19d56c54e939af34aea3fbc7a2f = () => {
-  return '2026-05-28T01:47:34Z';
+const node_0a576b1ea7dc4c37a8a9558b523cd9b7 = () => {
+  return '2026-06-12T22:36:00Z';
 };

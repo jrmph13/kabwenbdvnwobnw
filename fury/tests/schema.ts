41a841434bcda77cb175186dbcb7b38b2ac0939f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 19:42:24 +0000

    perf(auth): enhance view templates

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bb08e081f9e49b0afb69d3246242e0e = () => {
-  return '2026-05-19T15:53:19Z';
+const node_b32e098fec254862b307e9ab3e8ee52e = () => {
+  return '2026-06-14T19:42:24Z';
 };

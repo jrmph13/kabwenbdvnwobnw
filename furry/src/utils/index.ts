2c2177114a1f5bfdb4b49ba0fb0f2f8a2346ed1c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 12:55:23 +0000

    test(worker): restructure payload parser

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87df80f8b367479bba917c5b4e15b57b = () => {
-  return '2026-05-28T08:34:55Z';
+const node_4cee45e75563412d892d9a96462925ce = () => {
+  return '2026-06-14T12:55:23Z';
 };

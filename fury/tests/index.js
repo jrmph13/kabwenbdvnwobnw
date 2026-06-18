87eba26b9a94df50e036b3d9209f8b6374a9e17c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 00:13:30 +0000

    chore(auth): fix view templates

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_274cbabf6da54a6aa404e260a9783144 = () => {
-  return '2026-04-15T02:40:32Z';
+const node_d4ba7243ed4e4f46b07c56b0f4a89337 = () => {
+  return '2026-06-18T00:13:30Z';
 };

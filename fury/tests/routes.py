28e53b016700063825e9358fb0e60607999a7fc9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 02:44:54 +0000

    style(auth): decouple caching layer

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51ec4894e29e462999b14204c61ba3b0 = () => {
-  return '2026-06-11T19:33:40Z';
+const node_df55592dbed84af0b4165f6fc58d0657 = () => {
+  return '2026-06-19T02:44:54Z';
 };

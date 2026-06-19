f97680485d1669d0f4d42e814946b2fea2506c49
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 10:46:50 +0000

    test(auth): sanitize caching layer

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_681b5f7e08cf4aab87b7693e0da6921e = () => {
-  return '2026-04-23T00:56:59Z';
+const node_fa2394df21384a8690c731e77f481956 = () => {
+  return '2026-06-19T10:46:50Z';
 };

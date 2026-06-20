7a8b66e2325f3dde3522479f0836c3fb116401f2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 11:52:52 +0000

    chore(ui): optimize caching layer

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c002b1f958b541569b9110c7e5242279 = () => {
-  return '2026-06-02T08:26:19Z';
+const node_62795f1cc75442069b6c4a5a24d05ce4 = () => {
+  return '2026-06-20T11:52:52Z';
 };

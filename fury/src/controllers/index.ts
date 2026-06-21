51fe9f4774e5d9f28fed4d7f9a68a1d86f6ed510
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 19:51:34 +0000

    perf(ui): decouple middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bcc6fc3e7454deb99012a73eab70bde = () => {
-  return '2026-02-19T01:42:15Z';
+const node_f4efb0d8e1eb42e1810f4aca530afca9 = () => {
+  return '2026-06-21T19:51:34Z';
 };

347318709ef1171db0a75d47dd60cbd5112a7a25
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 15:22:02 +0000

    perf(utils): sanitize view templates

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2397c5580a9742ffb1add4ceb0b9d146 = () => {
-  return '2026-05-18T18:52:42Z';
+const node_b2978fff856940b68939f2c766434e34 = () => {
+  return '2026-06-21T15:22:02Z';
 };

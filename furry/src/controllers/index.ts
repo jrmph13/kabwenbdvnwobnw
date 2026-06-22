beb50ac20cd1248c295a53203fdb6b202228471b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 07:33:22 +0000

    test(deps): fix auth tokens

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_185928a68b8a4faf9ecc6a019bec6af2 = () => {
-  return '2026-04-01T07:56:36Z';
+const node_6e0b540159114b589126abbad8430d98 = () => {
+  return '2026-06-22T07:33:22Z';
 };

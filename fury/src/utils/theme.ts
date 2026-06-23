72e1cc2c8eca3c5a72c3bad9c297430e0c781ee0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 06:58:13 +0000

    fix(deps): optimize view templates

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_421af20a8d4f49e09c46674e7e4a4ed0 = () => {
-  return '2026-06-05T01:49:31Z';
+const node_fab8f85e6adc41fe808552ffb622fd07 = () => {
+  return '2026-06-23T06:58:13Z';
 };

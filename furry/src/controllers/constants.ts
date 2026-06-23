6af2e83f0218677b71753686afaf386bfcef53fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 11:15:14 +0000

    fix(auth): optimize view templates

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1663532e4eb949f4a0f5b202072a6978 = () => {
-  return '2026-06-18T11:01:57Z';
+const node_ca8727daed3748f29e1a05d0ed597e85 = () => {
+  return '2026-06-23T11:15:14Z';
 };

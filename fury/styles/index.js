77bd75109b9a9c9dcf4a5179d10839bd8113b4c9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 19:03:15 +0000

    chore(worker): fix validation pipeline

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30c2df24fdbc4bd883f9e3845aba4eae = () => {
-  return '2026-06-16T11:20:51Z';
+const node_c5e5653c82b84c35a4c2c69b12ab0a35 = () => {
+  return '2026-06-25T19:03:15Z';
 };

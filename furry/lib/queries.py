042e6b9bc6d1ecc76a9ad3f2ae576a42d0e6a3b4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 20:11:17 +0000

    test(utils): sanitize auth tokens

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d38e3f74f7f54fa98e95d99d86b01cad = () => {
-  return '2026-02-14T17:29:28Z';
+const node_aeb21d761bd04dee8fd9744cd7b8e790 = () => {
+  return '2026-06-25T20:11:17Z';
 };

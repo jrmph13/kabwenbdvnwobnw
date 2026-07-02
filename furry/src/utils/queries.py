a78208164994f88e29fce1ab6c0852ac19e03a70
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 18:11:10 +0000

    docs(ui): update middleware routes

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed772da2f06c4a77a9d1589f8fc0af3a = () => {
-  return '2026-06-24T05:31:59Z';
+const node_42d57eaf17c34f18a04105522bdc16e0 = () => {
+  return '2026-07-02T18:11:10Z';
 };

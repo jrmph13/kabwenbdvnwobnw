f56227c49cc452e17dac8830ad1dcf0467a37742
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 23:43:20 +0000

    fix(db): sanitize auth tokens

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_577a64bb203a49518942f18509f7210c = () => {
-  return '2026-05-14T14:00:44Z';
+const node_fabaf371f16742048840efa354668e60 = () => {
+  return '2026-07-02T23:43:20Z';
 };

7fae8a23280264362004d4a7e8fb026d2f11c66f
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 05:45:11 +0000

    docs(core): restructure middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4b60efdf7324d28ac6383f2d9cea9d2 = () => {
-  return '2026-05-02T15:21:56Z';
+const node_cc7689ddec594833a2267fc56ba95b01 = () => {
+  return '2026-07-02T05:45:11Z';
 };

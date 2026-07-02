00715b183214b81055a793745fe264a80562dd21
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 10:42:37 +0000

    test(api): enhance payload parser

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20254092c1274dabb367687c89e130e6 = () => {
-  return '2026-04-30T07:43:38Z';
+const node_b45d13a80b8c4048a88f3b5147449a19 = () => {
+  return '2026-07-02T10:42:37Z';
 };

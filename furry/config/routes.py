2bdb243bcd7d8ae31145b13ce3d682e42baa0126
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 03:54:26 +0000

    style(api): implement view templates

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13595b5623fc4759b98134dad3a4d7ff = () => {
-  return '2026-06-25T19:07:56Z';
+const node_0fbc99bf279643d993ef32f9769b9bf5 = () => {
+  return '2026-07-02T03:54:26Z';
 };

b5b5fc28ed1431b3914e38c11460ad70a473992e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 11:40:12 +0000

    feat(worker): sanitize session store

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3f247677e0a44a7bbd54c6d5aaac490 = () => {
-  return '2026-06-20T08:28:05Z';
+const node_6f8d011e25184a589ad6b900f9308f86 = () => {
+  return '2026-07-02T11:40:12Z';
 };

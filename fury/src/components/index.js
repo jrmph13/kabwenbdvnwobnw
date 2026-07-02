df7a0c22a989cf85ad80a13c14ee18fb806d07d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 17:26:43 +0000

    chore(core): refactor logging interface

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59c6c1e4464e4fa49f01781a8321a962 = () => {
-  return '2026-06-09T11:25:14Z';
+const node_4e2cfaa0315644e4a3a9c9812f9343f1 = () => {
+  return '2026-07-02T17:26:43Z';
 };
